2fba95712d713969e6737dd316673a79c869e1b6
seppestroobants <s.stroobants@hotmail.com>
2024-07-01 14:19:12 +0200

    created relation and added functionality

--- src/typeorm/orm.config.ts
+++ src/typeorm/orm.config.ts
@@ -8,7 +8,7 @@ export const ormConfig = {
   type: 'postgres',
   entities: [Tenant, Client, Project],
   migrations: [join(__dirname, '../migrations/**/*.{ts,js}')],
-  synchronize: false,
+  synchronize: true,
 } satisfies DataSourceOptions;
 
 export default ormConfig;
