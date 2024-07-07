e6aac19190a96f3d120d8efa4dfb3f72b1ba165b
seppestroobants <s.stroobants@hotmail.com>
2024-07-07 15:29:45 +0200

    pr changes

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -4,6 +4,6 @@ import { Client } from '../../clients/entities/client.entity';
 export class CreateProjectDto {
   @ApiProperty()
   name: string;
-  @ApiProperty({ type: Client, properties: { id: { type: 'number' } } })
-  client: { id: number };
+  @ApiProperty()
+  client: Client;
 }
