52b1f1a9b8799ba4c82e5434974774b1018249e1
Seppe Stroobants <s.stroobants@hotmail.com>
2024-08-11 13:38:45 +0200

    create file entity

---
+++ src/files/entities/file.entitiy.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class File {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+
+  @Column()
+  name: string;
+}
