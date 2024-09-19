817c68decdf510d9c314c2280b91f6e9218d20a5
Seppe Stroobants <s.stroobants@hotmail.com>
2024-09-19 09:15:49 +0200

    back to AWS S3 bucket

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,51 +1,41 @@
-import { Injectable, Scope } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateFileDto } from './dto/create-file.dto';
 import { UpdateFileDto } from './dto/update-file.dto';
 import { InjectTenantRepository } from '../tenancy/tenancy.decorators';
 import { Repository } from 'typeorm';
 import { File } from './entities/file.entity';
 import { ConfigService } from '@nestjs/config';
-import { Client as MinioClient } from 'minio';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
-@Injectable({ scope: Scope.REQUEST })
+@Injectable()
 export class FilesService {
-  private readonly minioClient: MinioClient;
+  private readonly s3Client = new S3Client({
+    region: this.configService.getOrThrow<string>('AWS_S3_REGION'),
+  });
 
   constructor(
     @InjectTenantRepository(File)
     private readonly filesRepository: Repository<File>,
     private readonly configService: ConfigService,
-  ) {
-    // Initialize MinIO client using environment variables
-    this.minioClient = new MinioClient({
-      endPoint: this.configService.getOrThrow<string>('MINIO_ENDPOINT'),
-      port: parseInt(this.configService.getOrThrow<string>('MINIO_PORT')),
-      useSSL: this.configService.getOrThrow<string>('MINIO_USE_SSL') === 'true',
-      accessKey: this.configService.getOrThrow<string>('MINIO_ACCESSKEY'),
-      secretKey: this.configService.getOrThrow<string>('MINIO_SECRETKEY'),
-    });
-  }
+  ) {}
 
   async uploadFile(
     fileName: string,
     fileBuffer: Buffer,
     fileDto: CreateFileDto,
-  ): Promise<File> {
-    const bucketName = this.configService.getOrThrow<string>('MINIO_BUCKET');
+  ) {
+    const bucketName =
+      this.configService.getOrThrow<string>('AWS_S3_BUCKET_NAME');
+    const uploadParams = {
+      Bucket: bucketName,
+      Body: fileBuffer,
+      Key: fileName,
+    };
+
     try {
-      // Ensure the bucket exists
-      const bucketExists = await this.minioClient.bucketExists(bucketName);
-      if (!bucketExists) {
-        await this.minioClient.makeBucket(bucketName, '');
-      }
+      await this.s3Client.send(new PutObjectCommand(uploadParams));
+      const fileUri = this.getS3FileUri(bucketName, fileName);
 
-      // Upload the file to MinIO
-      await this.minioClient.putObject(bucketName, fileName, fileBuffer);
-
-      // Generate the file URI
-      const fileUri = this.getMinioFileUri(bucketName, fileName);
-
-      // Save file details in the database
       const newFile = this.filesRepository.create({
         ...fileDto,
         name: fileName,
@@ -53,16 +43,14 @@ export class FilesService {
       });
       return await this.filesRepository.save(newFile);
     } catch (error) {
-      throw new Error(`Failed to upload file to MinIO: ${error}`);
+      throw new Error(`Failed to upload file to S3: ${error}`);
     }
   }
 
-  // Generate a MinIO file URL
-  private getMinioFileUri(bucketName: string, fileName: string): string {
-    const endpoint = this.configService.getOrThrow<string>('MINIO_ENDPOINT');
-    const port = this.configService.getOrThrow<string>('MINIO_PORT');
-    return `http://${endpoint}:${port}/${bucketName}/${fileName}`;
+  private getS3FileUri(bucketName: string, fileName: string) {
+    return `https://${bucketName}.s3.amazonaws.com/${fileName}`;
   }
+
   findAll() {
     return this.filesRepository.find();
   }
