6840a9bc6602d10b2d46bfd039cfc9ff2b40d156
Seppe Stroobants <s.stroobants@hotmail.com>
2024-11-21 15:20:40 +0100

    trying to add auth

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -6,16 +6,21 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
-import { ApiHeader } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiHeader, ApiResponse } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
 @ApiHeader({
   name: 'x-tenant-id',
   description: 'Tenant ID',
 })
+@ApiBearerAuth('zitadel-jwt')
+@ApiResponse({ status: 401, description: 'Unauthorized' })
+@UseGuards(AuthGuard('zitadel'))
 @Controller('clients')
 export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,107 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import {
+  ValidationPipe,
+  ValidationPipeOptions,
+  ClassSerializerInterceptor,
+  NestApplicationOptions,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as packageJson from '../package.json';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  const config = new DocumentBuilder()
+  const appOptions: NestApplicationOptions = {
+    logger: ['error', 'warn', 'debug', 'log'],
+  };
+
+  const app = await NestFactory.create(AppModule, appOptions);
+
+  // Enable CORS
+  const corsOptions = {};
+  app.enableCors(corsOptions);
+
+  // Set up validation globally
+  const validationOptions: ValidationPipeOptions = {
+    transform: true,
+    always: true,
+    forbidUnknownValues: true,
+  };
+  app.useGlobalPipes(new ValidationPipe(validationOptions));
+
+  // Set up global interceptors
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  // Set global prefix and versioning
+  const globalApiPrefix = '/api';
+  app.setGlobalPrefix(globalApiPrefix);
+  app.enableVersioning();
+
+  // Get ConfigService to load environment variables
+  const config: ConfigService = app.get(ConfigService);
+
+  // Fetch required configuration values
+  const port: number = config.getOrThrow<number>('APP_PORT');
+  const clientId: string = config.getOrThrow<string>('OPENAPI_CLIENT_ID');
+  const clientSecret: string = config.getOrThrow<string>(
+    'OPENAPI_CLIENT_SECRET',
+  );
+  const authority: string = config.getOrThrow<string>('IDP_AUTHORITY');
+
+  // Define scopes and redirect URI
+  const scopes: string[] = ['openid', 'profile', 'email', 'offline_access'];
+  let redirectUri: string;
+
+  // Set up Swagger documentation
+  const swaggerDocument = new DocumentBuilder()
     .setTitle('AS-BUILT API')
     .setDescription('The AS-BUILT API description')
-    .setVersion('1.0')
-    .build();
+    .setVersion(packageJson.version)
+    .setTermsOfService('http://swagger.io/terms/')
+    .setExternalDoc('Find out more about Swagger', 'http://swagger.io/')
+    .setContact('Developer', '', 'mail@example.com')
+    .setLicense('Apache 2.0', 'http://www.apache.org/licenses/LICENSE-2.0.html')
+    .addSecurity('zitadel-jwt', {
+      type: 'openIdConnect',
+      openIdConnectUrl: `${authority}/.well-known/openid-configuration`,
+      name: 'Zitadel',
+    });
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // Set environment-specific configurations
+  if (config.get<string>('NODE_ENV') !== 'production') {
+    swaggerDocument.addServer(`http://localhost:${port}`);
+    redirectUri = `http://localhost:${port}`;
+  } else {
+    redirectUri = 'YOUR PROD URL HERE';
+    throw new Error('SET YOUR PROD URL HERE AND REMOVE THIS THROW');
+  }
 
-  await app.listen(3000);
+  // Generate Swagger document
+  const document = SwaggerModule.createDocument(app, swaggerDocument.build());
+  SwaggerModule.setup(globalApiPrefix.slice(1), app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      oauth2RedirectUrl: `${redirectUri}${globalApiPrefix}/oauth2-redirect.html`,
+      initOAuth: {
+        clientId,
+        clientSecret,
+        scopes,
+      },
+    },
+  });
+
+  // Save Swagger JSON in non-production environments
+  if (config.get<string>('NODE_ENV') !== 'production') {
+    fs.writeFileSync(
+      path.join(__dirname, '..', 'swagger.json'),
+      JSON.stringify(document),
+    );
+  }
+
+  // Start the application
+  await app.listen(port);
 }
-bootstrap();
+
+bootstrap().then();
