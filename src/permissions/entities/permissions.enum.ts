e0f36ec36c65087dbc9fe460163df716b2f404c4
Seppe Stroobants <s.stroobants@hotmail.com>
2024-12-09 09:45:07 +0100

    added all api endpoints permissions

--- src/permissions/entities/permissions.enum.ts
+++ src/permissions/entities/permissions.enum.ts
@@ -1,11 +1,44 @@
 export enum PermissionName {
-  PROJECT_CREATE = 'project:create',
-  PROJECT_EDIT = 'project:edit',
-  PROJECT_VIEW = 'project:view',
-  PROJECT_DELETE = 'project:delete',
+  //Client
   CLIENT_CREATE = 'client:create',
   CLIENT_EDIT = 'client:edit',
   CLIENT_VIEW = 'client:view',
   CLIENT_DELETE = 'client:delete',
+  //Comment
+  COMMENT_CREATE = 'comment:create',
+  COMMENT_EDIT = 'comment:edit',
+  COMMENT_VIEW = 'comment:view',
+  COMMENT_DELETE = 'comment:delete',
+  //Files
+  FILE_CREATE = 'file:create',
+  FILE_EDIT = 'file:edit',
+  FILE_VIEW = 'file:view',
+  FILE_DELETE = 'file:delete',
+  //Permissions
+  PERMISSION_CREATE = 'permission:create',
+  PERMISSION_EDIT = 'permission:edit',
+  PERMISSION_VIEW = 'permission:view',
+  PERMISSION_DELETE = 'permission:delete',
+  //Project
+  PROJECT_CREATE = 'project:create',
+  PROJECT_EDIT = 'project:edit',
+  PROJECT_VIEW = 'project:view',
+  PROJECT_DELETE = 'project:delete',
+  //Revision
+  REVISION_CREATE = 'revision:create',
+  REVISION_EDIT = 'revision:edit',
+  REVISION_VIEW = 'revision:view',
+  REVISION_DELETE = 'revision:delete',
+  //Tenant
+  TENANT_CREATE = 'tenant:create',
+  TENANT_EDIT = 'tenant:edit',
+  TENANT_VIEW = 'tenant:view',
+  TENANT_DELETE = 'tenant:delete',
+  //User
+  USER_CREATE = 'user:create',
+  USER_EDIT = 'user:edit',
+  USER_VIEW = 'user:view',
+  USER_DELETE = 'user:delete',
+
   //more permissions need to be added in the future
 }
