fd563aec77c376547a920875b30663cfe1f94b64
Seppe Stroobants <s.stroobants@hotmail.com>
2024-12-16 14:50:26 +0100

    changed e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,29 +1,27 @@
+process.env.OPENAPI_CLIENT_ID = 'test-client-id';
+process.env.OPENAPI_CLIENT_SECRET = 'test-client-secret';
+process.env.IDP_AUTHORITY = 'http://test-idp-authority';
+process.env.IDP_AUTHORIZATION_PROFILE_KEY_ID = 'test-key-id';
+process.env.IDP_AUTHORIZATION_PROFILE_KEY = 'test-key';
+process.env.IDP_AUTHORIZATION_PROFILE_APP_ID = 'test-app-id';
+process.env.IDP_AUTHORIZATION_PROFILE_CLIENT_ID = 'test-client-id';
+
+import * as process from 'node:process';
+import { TestEnvironmentGlobals } from './testEnvironment';
+
+const globals = global as typeof globalThis & TestEnvironmentGlobals;
+process.env.DATABASE_URL = globals.postgresContainer.getConnectionUri();
+process.env.STORAGE_ENDPOINT_URL = globals.minioContainer.getConnectionUri();
+
+// Import after environment variables are set
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import * as process from 'node:process';
-import { TestEnvironmentGlobals } from './testEnvironment';
 import { AuthGuard } from '@nestjs/passport';
-
-const globals = global as typeof globalThis & TestEnvironmentGlobals;
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-
-  beforeAll(async () => {
-    process.env.OPENAPI_CLIENT_ID = 'test-client-id';
-    process.env.OPENAPI_CLIENT_SECRET = 'test-client-secret';
-    process.env.IDP_AUTHORITY = 'http://test-idp-authority';
-    process.env.IDP_AUTHORIZATION_PROFILE_KEY_ID = 'test-key-id';
-    process.env.IDP_AUTHORIZATION_PROFILE_KEY = 'test-key';
-    process.env.IDP_AUTHORIZATION_PROFILE_APP_ID = 'test-app-id';
-    process.env.IDP_AUTHORIZATION_PROFILE_CLIENT_ID = 'test-client-id';
-
-    process.env.DATABASE_URL = globals.postgresContainer.getConnectionUri();
-    process.env.STORAGE_ENDPOINT_URL =
-      globals.minioContainer.getConnectionUri();
-  });
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -39,9 +37,8 @@ describe('AppController (e2e)', () => {
   afterEach(async () => {
     if (app) {
       await app.close();
- 
     }
-    });
+  });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
