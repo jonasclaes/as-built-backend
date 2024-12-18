1817b5a665c03146cea7fc427284e9b250019283
Jonas Claes <jonas@jonasclaes.be>
2024-12-18 18:21:52 +0100

    test: fix end-to-end tests by changing app to a dynamic module

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,26 +3,48 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import * as process from 'node:process';
-import { TestEnvironmentGlobals } from './testEnvironment';
 import { AuthGuard } from '@nestjs/passport';
-import * as dotenv from 'dotenv';
-
-const globals = global as typeof globalThis & TestEnvironmentGlobals;
-
-dotenv.config({ path: '.env.test' });
+import {
+  PostgreSqlContainer,
+  StartedPostgreSqlContainer,
+} from '@testcontainers/postgresql';
+import { MinIOContainer, StartedMinIOContainer } from './minioContainer';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
+  let postgresContainer: StartedPostgreSqlContainer;
+  let minioContainer: StartedMinIOContainer;
+
   beforeAll(async () => {
-    process.env.DATABASE_URL = globals.postgresContainer.getConnectionUri();
-    process.env.STORAGE_ENDPOINT_URL =
-      globals.minioContainer.getConnectionUri();
+    postgresContainer = await new PostgreSqlContainer().start();
+    minioContainer = await new MinIOContainer().start();
+
+    process.env.NODE_ENV = 'test';
+    process.env.APP_PORT = '3000';
+
+    process.env.OPENAPI_CLIENT_ID = 'test-client-id';
+    process.env.OPENAPI_CLIENT_SECRET = 'test-client-secret';
+    process.env.IDP_AUTHORITY = 'http://test-idp-authority';
+    process.env.IDP_AUTHORIZATION_TYPE = 'jwt-profile';
+    process.env.IDP_AUTHORIZATION_PROFILE_TYPE = 'application';
+    process.env.IDP_AUTHORIZATION_PROFILE_KEY_ID = 'test-key-id';
+    process.env.IDP_AUTHORIZATION_PROFILE_KEY = 'test-key';
+    process.env.IDP_AUTHORIZATION_PROFILE_APP_ID = 'test-app-id';
+    process.env.IDP_AUTHORIZATION_PROFILE_CLIENT_ID = 'test-client-id';
+
+    process.env.DATABASE_URL = postgresContainer.getConnectionUri();
+    process.env.STORAGE_ENDPOINT_URL = minioContainer.getConnectionUri();
+  }, 240_000);
+
+  afterAll(async () => {
+    await minioContainer.stop();
+    await postgresContainer.stop();
   });
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule.register()],
     })
       .overrideGuard(AuthGuard('zitadel'))
       .useValue({ canActivate: () => true })
