aa4ef10859c16b13341c3676f7e70324793f1d7b
Seppe Stroobants <113921218+SeppeStroobants@users.noreply.github.com>
2024-12-19 20:26:45 +0100

    4 as a user i can edit my change my password so that i can login using my new password ss (#20)
    * Update users
    
    * adding default password
    
    * request password changes (email) using Zitadel
    
    * pr changes
    
    * e2e fixing
    
    * request change password
    
    * password reset
    
    * e2e fail
    
    * change password
    
    * small change to CD pipeline

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -13,13 +13,13 @@ export class UsersService {
     private readonly configService: ConfigService,
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
+
   url = this.configService.getOrThrow<string>('IDP_AUTHORITY');
+  serviceUserToken = this.configService.get<string>(
+    'ZITADEL_SERVICE_USER_TOKEN',
+  );
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const serviceUserToken = this.configService.get<string>(
-      'ZITADEL_SERVICE_USER_TOKEN',
-    );
-
     const { givenName, familyName, email } = createUserDto;
     const payload = {
       profile: {
@@ -35,15 +35,17 @@ export class UsersService {
     try {
       const response = await axios.post(`${this.url}/v2/users/human`, payload, {
         headers: {
-          Authorization: `Bearer ${serviceUserToken}`,
+          Authorization: `Bearer ${this.serviceUserToken}`,
           'Content-Type': 'application/json',
         },
       });
 
       const { userId } = response.data;
 
-      const user = this.userRepository.create({ uid: userId });
-      return await this.userRepository.save(user);
+      const user = new User();
+      user.uid = userId;
+      await this.userRepository.save(user);
+      return user;
     } catch (error) {
       console.error(
         'API call error:',
@@ -62,6 +64,7 @@ export class UsersService {
     }
 
     const { givenName, familyName, email } = updateUserDto;
+
     const payload = {
       username: `${email}`,
       profile: {
@@ -81,9 +84,7 @@ export class UsersService {
         payload,
         {
           headers: {
-            Authorization: `Bearer ${this.configService.get<string>(
-              'ZITADEL_SERVICE_USER_TOKEN',
-            )}`,
+            Authorization: `Bearer ${this.serviceUserToken}`,
             'Content-Type': 'application/json',
           },
         },
@@ -96,6 +97,39 @@ export class UsersService {
       );
       throw new Error(
         error.response?.data?.message || 'Failed to update user in ZITADEL API',
+      );
+    }
+  }
+
+  async requestPasswordReset(uid: string) {
+    const user = await this.userRepository.findOne({ where: { uid } });
+    if (!user) {
+      throw new Error('User not found in database');
+    }
+    const payload = {
+      sendLink: {
+        notificationType: 'NOTIFICATION_TYPE_Email',
+      },
+    };
+    try {
+      const response = await axios.post(
+        `${this.url}/v2/users/${uid}/password_reset`,
+        payload,
+        {
+          headers: {
+            Authorization: `Bearer ${this.serviceUserToken}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      return response.data;
+    } catch (error) {
+      console.error(
+        'API call error:',
+        error.response ? error.response.data : error.message,
+      );
+      throw new Error(
+        error.response?.data || 'Failed to reset user password in ZITADEL API',
       );
     }
   }
